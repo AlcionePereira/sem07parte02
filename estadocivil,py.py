fc22935e4ac2a7420a64bda24b2cf8e4e921ca35
AlcionePereira <90060154+AlcionePereira@users.noreply.github.com>
2021-10-15 21:11:41 -0300

    Add files via upload

---
+++ estadocivil,py.py
@@ -0,0 +1,20 @@
+def situação_amorosa(solteiro,casado):
+
+        if estado_civil == 1:
+            conjuge = input().strip()
+            'nome' + 'conjuge'
+            print(len(f'{nome}{conjuge}'))
+
+        elif estado_civil == 2:
+            print(len(f'{nome}'))
+    
+nome = input().strip()             
+estado_civil = int(input().strip())
+
+
+resultado = situação_amorosa(nome,estado_civil)
+
+        
+
+    
+        
